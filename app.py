f296fc70cf78efaf5511e8d17edfcff904ab836d
SC7RED <delenyand@gmail.com>
2024-09-19 20:51:18 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,79 @@
+import math
+
+#name = input('What is your name? '
+#favColor = input('What is your favorite color? ')
+#print(name + ' likes ' + favColor)
+
+#birth_year = input('Birth year: ')
+#print(type(birth_year))
+#age = 2024 - int(birth_year)
+#print(type(age))
+#print(age)
+
+#weight_lbs = input('What is your weight in pounds? ')
+#weight_kg = int(weight_lbs) / 2.20462
+#print(weight_kg)
+
+#course = '''
+#Hi John,
+
+#Here is our first email to you.
+
+#Thank you,
+# Support Team '''
+#print(course)
+
+#course1 = 'Python for Beginners'
+#another = course1[1:]
+
+#print(another)\
+
+
+#first = 'John'
+#last = 'Smith'
+#msg = f'{first} [{last}] is a coder'
+#print(msg)
+
+
+#course2 = 'Python for Beginners'
+#len() index length
+#print(course2.upper()) | all uppercase
+#print(course2.lower()) | all lowercase
+#print(course2)
+#print(course2.replace('Beginners', 'Absolute Beginners'))
+#print('python' in course2)
+
+#x = 2.9
+#print(round(x))
+#print(abs(-1 * x))
+
+#print(math.ceil(2.9))
+#print(math.floor(2.9))
+
+
+#is_hot = False
+#is_cold = False
+
+#if is_hot:
+#    print("It's a hot day")
+#    print("Drink plenty of water")
+#elif is_cold:
+#    print("It's a cold day")
+#    print("Wear warm clothes")
+#else:
+#    print("It's a lovely day")
+#print("Enjoy your day")
+
+
+#priceHouse = 1000000
+#buyerGoodCredit = True
+
+#if buyerGoodCredit:
+#    downpaymentPercent = 0.1
+#else:
+#    downpaymentPercent = 0.2
+
+#downpayment = downpaymentPercent * priceHouse
+#print(f'Your down payment will be ${downpayment}')
+
+
